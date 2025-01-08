47e68e8ce149d1589afc16fbb37b41722f039a94
dong-junn <djl020326@gmail.com>
2025-01-08 12:52:53 +0900

    문제1-12 완료 : 암호

---
+++ src/problem1/Problem1_12.java
@@ -0,0 +1,24 @@
+package problem1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem1_12 {
+
+    public String solution(String str, int quantity) {
+        str = str.replace('#', '1');
+        str = str.replace('*', '0');
+        char[] arr = new char[quantity];
+        for (int i = 0; i < quantity; i++) {
+            arr[i] = (char)Integer.parseInt(str.substring(i * 7, i * 7 + 7), 2);
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int quantity = sc.nextInt();
+        String str = sc.next();
+        System.out.println(new Problem1_12().solution(str, quantity));
+    }
+}
